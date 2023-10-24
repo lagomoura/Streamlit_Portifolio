4b32ffccf664048c2a6176d45383c922f8b6b6f2
lagomoura <mouralago@live.com>
2023-10-24 00:44:09 -0300

    folders structure improved

--- home.py
+++ home.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.write("HEllo Test")
+st.set_page_config(
+  page_title="Gustavo Lago Portfolio",
+  page_icon="👋"
+)
+
+st.title("Bienvenidx a mi portfólio")
+st.sidebar.success("Seleccione una opción abajo: ")
+
+st.write("Hello Test")
